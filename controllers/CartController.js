b9fc594be1e96be6200795282d90c9d94627d706
kal-kidan <kal05627356@gmail.com>
2020-12-01 07:06:29 -0500

    update and delete cart function added to the controller

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -23,8 +23,41 @@ const addToCart = async (req, res, next)=>{
     }
  }
 
+ const deleteCart = async (req, res, next)=>{ 
+    try { 
+     let {_id} = req.params
+     let carT = await cart.findOneAndRemove({_id,  customer: req.user._id}, {useFindAndModify: false})
+     if(!carT){
+       return res.status(404).json({error: true, msg: `cart with id ${_id} is not found`})
+     }
+     return res.json({status: true, msg: `the cart is deleted successfuly`})
+    } catch (error) { 
+        return res.status(400).json({error: true, msg:"please enter valid id"})
+    }
+ }
+ 
+ const updateCart = async (req, res, next)=>{ 
+   try { 
+    let {_id} = req.params
+    let {quantity} = req.body
+    if(!quantity>0){
+        return res.status(400).json({error: true, msg: 'please enter valid quantity'})
+    }
+    let carT = await cart.findOneAndUpdate({_id, customer: req.user._id}, quantity, {useFindAndModify: false})
+    if(!carT){
+      return res.status(404).json({error: true, msg: `cart with id of ${_id} is not found`})
+    }
+    return res.json({status: true, msg: `you have updated cart quantity successfuly`})
+   } catch (error) { 
+       return res.status(400).json({error: true, msg:"please enter valid id"})
+   }
+ }
+
+ 
  module.exports = {
     addToCart,
-    getCarts
+    deleteCart,
+    getCarts,
+    updateCart
  }
  
